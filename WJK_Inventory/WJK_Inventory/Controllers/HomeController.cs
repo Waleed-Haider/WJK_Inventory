f6a29d151a08a03acd11e47a3c5382a494f12462
WaleedHaider <hafizwaleedfarooq@gmail.com>
2018-05-23 18:30:02 +0500

    Theme Integrated

---
+++ WJK_Inventory/WJK_Inventory/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WJK_Inventory.Models;
+
+namespace WJK_Inventory.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ItemDbContext myItemDbContext = new ItemDbContext();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Item item)
+        {
+            return View();
+        }
+    }
+}
---
+++ WJK_Inventory/WJK_Inventory/Models/ItemDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WJK_Inventory.Models
+{
+    public class ItemDbContext : DbContext
+    {
+        public ItemDbContext():base("WjkDb")
+        {
+            
+        }
+        public DbSet<Item> Items { get; set; }
+
+    }
+}
